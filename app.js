a1c4c252271bebc4ad6b907888a95cf0c5653f71
ChrisEspi110 <christopherespinal110@gmail.com>
2025-10-29 00:59:00 -0400

    sud

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pool = require('./db'); 
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
